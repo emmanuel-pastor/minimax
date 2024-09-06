ebbb8ec41f730f44fbba3e2121989eb698061244
Emmanuel Pastor <ommanu13@hotmail.fr>
2024-09-07 01:02:25 +0200

    Add game choice in cli main
    Signed-off-by: Emmanuel Pastor <ommanu13@hotmail.fr>

--- src/main/kotlin/frontends/cli/Main.kt
+++ src/main/kotlin/frontends/cli/Main.kt
@@ -1,7 +1,18 @@
 package frontends.cli
 
+import frontends.cli.connect_four.ConnectFourCli
 import frontends.cli.tic_tac_toe.TicTacToeCli
 
 fun main() {
-    TicTacToeCli().run()
+    do {
+        println("1: Tic Tac Toe")
+        println("2: Connect Four")
+        print("Choose a game: ")
+        val input = readlnOrNull()
+        when (input) {
+            "1" -> TicTacToeCli().run()
+            "2" -> ConnectFourCli().run()
+            else -> println("Invalid input")
+        }
+    } while (true)
 }
